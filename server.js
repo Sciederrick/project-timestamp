1257889fc0f8f1544e99f75529921f280f5767c4
Sciederrick <derrickmbarani@gmail.com>
2020-07-20 00:01:11 +0300

    initial commit

--- server.js
+++ server.js
@@ -2,31 +2,45 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+require('dotenv').config()
+let express = require('express')
+let moment = require('moment')
+let app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+let cors = require('cors')
+app.use(cors({optionSuccessStatus: 200}))  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+  res.sendFile(__dirname + '/views/index.html')
+})
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// timestamp endpoint
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let date_string = req.params.date_string
+  // validate for correct date input -->2011-10-10T14:48:00  OR -->2011-10-10
+  if (moment(date_string, "YYYY-MM-DDTHH:mm:ss", true).isValid() || moment(date_string, "YYYY-MM-DD", true).isValid()) {
+    let date = new Date(date_string) || new Date()
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    })
+  } else {
+    res.json({
+      "error": "Invalid Date"
+    })
+  }
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+let listener = app.listen(process.env.PORT||5000, function () {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
